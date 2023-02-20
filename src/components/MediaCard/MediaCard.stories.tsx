43a23c112c861af3571940d01f8cfe3d8d0ed541
Jeremy Viallatt <shoo>
2023-02-20 18:26:00 +0100

    fix(mediatype): fix missing userUID in MediaCard test and stories

--- src/components/MediaCard/MediaCard.stories.tsx
+++ src/components/MediaCard/MediaCard.stories.tsx
@@ -24,6 +24,7 @@ const mediaDefault: Media = {
   But Sanko is still a teenage dude with his own life, and he isn't about to let every monstrous lowlife in the galaxy rip him limb from limb.
   He and his skeletal buddy Avakian will use their dark powers to fend off any murder attempts while they search space for whomever put this curse on Sanko's bones...because killing them might end the madness.
   (And then Sanko can celebrate with his favorite spaghetti.) Don't miss this hilariously twisted and gruesome new series from the unique mind of Q Hayashida, creator of the manga and Netflix anime Dorohedoro!`,
+  userUID: '123',
 };
 
 export const Default = Template.bind({});
